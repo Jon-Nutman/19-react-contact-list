3099a6ad24821e1ede192b3a7e768474ed596135
Jon-Nutman <jonnut730@gmail.com>
2021-02-11 16:40:20 -0500

    connected

--- contact/src/pages/ContactDetail.js
+++ contact/src/pages/ContactDetail.js
@@ -3,7 +3,7 @@ import { useState } from 'react'
 
 export default function ContactDetail(props) {
     const idNum = props.match.params.id
-    const[activeUser, setActiveUser] = useState(contactJSON[idNum])
+    const[activeUser, setActiveUser] = useState(contactJSON.find(user => user.id == idNum))
    console.log(activeUser)
     return <div>{activeUser.name.first} {activeUser.name.last}</div>
 }
